e18fc7b4ab62d4c87bdc97f8c1102fce4e8ef26a
leaop <leaoneto_7@hotmail.com>
2024-03-06 00:22:08 +0000

    Adicionando código Python e gráfico gerado na branch develop

--- gasolina.py
+++ gasolina.py
@@ -1,13 +1,11 @@
+
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("gasolina.csv")
+df = pd.read_csv('gasolina.csv')
 
-plt.figure(figsize=(10, 6))
-sns.lineplot(data=df, x='dia', y='venda')
-plt.title('Preço Médio de Venda da Gasolina em São Paulo - Julho de 2021')
-plt.xlabel('Dia de Julho')
-plt.ylabel('Preço de Venda (R$)')
-plt.grid(True)
-plt.savefig("gasolina.png")
+plt.plot(df['dia'], df['venda'])
+plt.xlabel('Dia')
+plt.ylabel('Preço de Venda')
+plt.title('Preço Médio de Venda da Gasolina em São Paulo - Julho 2021')
+plt.show()
